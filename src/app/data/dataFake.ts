a31bc9d99db2826d12378a189d33c8528942ec2f
JanineDiniz <janine.diniz@outlook.com>
2023-09-28 20:30:45 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/AD72115B9CDAE873B2234BD0908CEF7B45A2D9AD9C66C699239B46E582F70A8B/scale?width=1200&aspectRatio=1.78&format=jpeg",
+    "title": "NOVO HOMEM DE FERRO EM 3D",
+    "description":"Marvel anuncia um novo filme do homem de ferro"
+  },
+  {
+    "id":"2",
+    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/28A21FFE0778495521587A0A0657E0415C2FDB2739AFF2A535FA8FAE6C95D1BD/scale?width=1200&aspectRatio=1.78&format=jpeg",
+    "title": "Nova série anunciada no Disney +",
+    "description":"bye"
+  }
+]
